ba4b2e65ead20ccd632ae6fbcedb9fc934688379
Gengo250 <miguelgengo28@gmail.com>
2026-03-18 23:42:40 -0300

    first

---
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+
+class MyNode(Node):
+    def __init__(self):
+        super().__init__('my_first_node')
+        self.counter = 0
+        self.get_logger().info('Nó iniciado com sucesso!')
+
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info('Hello ROS2 ' + str(self.counter))
+        self.counter += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
