dc7ea60e8c1510c8c465968a94c09b360bdea3f9
Gengo250 <miguelgengo28@gmail.com>
2026-03-21 20:21:13 -0300

    my new subscriber

---
+++ src/my_py_pkg/my_py_pkg/smartphone.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+
+class Smartphone(Node):
+    def __init__(self):
+        super().__init__('smartphone')
+
+        self.subscription = self.create_subscription(
+            String,
+            'robot_news',
+            self.callback_robot_news,
+            10
+        )
+
+        self.get_logger().info('Smartphone node has been started.')
+
+    def callback_robot_news(self, msg):
+        self.get_logger().info(f'Received message: {msg.data}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Smartphone()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
